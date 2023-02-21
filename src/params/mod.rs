326d120d3e115298a56f3cd707adad9a860bc076
Pakorn Nathong <pakorn.nth@hotmail.com>
2023-02-21 21:01:34 +0700

    feat: add tests for matching hash result to circomlib

--- src/params/mod.rs
+++ src/params/mod.rs
@@ -18,3 +18,42 @@ where
         .map(|e| F::from_str(e).unwrap())
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use ark_bn254::Fr;
+    use ark_crypto_primitives::crh::TwoToOneCRH;
+    use ark_ff::{to_bytes, One, Zero};
+
+    use crate::{MiMC, CRH};
+
+    use super::{
+        mimc_220_3_bn254::{MIMC_220_3_BN254_PARAMS, MIMC_220_3_BN254_ROUND_KEYS},
+        round_keys_contants_to_vec,
+    };
+
+    #[test]
+    fn correct_hash_result_params() {
+        let param = MiMC::<Fr, MIMC_220_3_BN254_PARAMS>::new(
+            Fr::zero(),
+            round_keys_contants_to_vec(&MIMC_220_3_BN254_ROUND_KEYS),
+        );
+
+        let result = <CRH<Fr, MIMC_220_3_BN254_PARAMS> as TwoToOneCRH>::evaluate(
+            &param,
+            &to_bytes!(Fr::one()).unwrap(),
+            &to_bytes!(Fr::zero()).unwrap(),
+        )
+        .unwrap();
+
+        assert_eq!(
+            result,
+            Fr::from_str(
+                "13403990812567987967336759851318987973794445269548215402779394294754792373527"
+            )
+            .unwrap()
+        );
+    }
+}
